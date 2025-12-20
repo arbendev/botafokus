3d84e278816ebc9d21ef3b307b3ea3192e387ef7
Arben Mehmetaj <amehmetaj@gmail.com>
2025-12-20 23:49:05 +0100

    Custom Editorial Backend

---
+++ app/Livewire/Editor/AiJobLogIndex.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Editor;
+
+use Livewire\Component;
+
+class AiJobLogIndex extends Component
+{
+    public function render()
+    {
+        return view('livewire.editor.ai-job-log-index');
+    }
+}
---
+++ app/Livewire/Editor/SourceSubmit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Editor;
+
+use Livewire\Component;
+
+class SourceSubmit extends Component
+{
+    public function render()
+    {
+        return view('livewire.editor.source-submit');
+    }
+}
